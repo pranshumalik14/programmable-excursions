d5d865c37c10b012389c7db9c109dd033c4fd043
Pranshu Malik <pranshumalik14@gmail.com>
2020-12-03 06:31:08 -0500

    adding optimal cricket

---
+++ optimal_cricket/cricket_and_optimization.jl
@@ -0,0 +1,15 @@
+### A Pluto.jl notebook ###
+# v0.12.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ ac4ce024-355a-11eb-0849-17028f5f519c
+md"
+
+# Cricket through lens of optimization
+
+"
+
+# ╔═╡ Cell order:
+# ╟─ac4ce024-355a-11eb-0849-17028f5f519c
