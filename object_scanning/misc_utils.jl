098d3d8f53830698fab00fdc8f5144b97abc050b
Pranshu Malik <pranshumalik14@gmail.com>
2020-12-27 16:39:53 -0500

    adding pose algebra (except dot operator)

--- object_scanning/misc_utils.jl
+++ object_scanning/misc_utils.jl
@@ -6,6 +6,7 @@ other objects/obstacles in the way.
 
 using Plots
 using Parameters
+using LinearAlgebra
 
 
 """
@@ -14,58 +15,152 @@ Types and struct definitions for holding map and pose information
 
 abstract type GeometricEntity end
 abstract type AbstractPose <: GeometricEntity end
+abstract type AbstractFrame <: GeometricEntity end
 abstract type AbstractPoint <: GeometricEntity end
 
 # holds 2D map and its properties to hold the (sliced) object and environment information
-@with_kw mutable struct Map
-    map::Matrix{Real}
-    res::Real
-    low::Real
-    high::Real
-    und::Real
+@with_kw mutable struct Map{T <: Real,F <: AbstractFloat}
+    map::Matrix{T}
+    low::T
+    high::T
+    und::T
+    res::F
 end
 
-# represents a 2D frame, (implicitly) treating it as a pose wrt world frame. this entity is
-# immutable.
-@with_kw struct Frame2 <: AbstractPose
-    x::Real
-    y::Real
-    θ::Real
-    name::AbstractString
+# represents a 2D frame, (implicitly) wrt world frame. a frame is immutable, and by
+# default, defined wrt the (right-handed) world  coordinate frame.  note that, by
+# convention, θ (rad) increases in the anticlockwise direction. the 2D frame entity lives
+# in SE(2).
+@with_kw struct Frame2{T <: Real,S <: AbstractString} <: AbstractFrame
+    x::T
+    y::T
+    θ::T
+    name::S
 end
 
-# constant global struct for world coordinate frame
-const 𝑊 = Frame2(0.0, 0.0, 0.0, "World")
+# returns a world coordinate frame with origin of type T
+𝑊(T) = Frame2{T,String}(0, 0, 0, "World")
 
-# holds 2D point information; a vector
-@with_kw mutable struct Point2 <: AbstractPoint
-    x::Real
-    y::Real
-    𝑢::Frame2 = 𝑊
+# holds 2D point information; a 2D bounded vector wrt frame {𝑈}
+@with_kw mutable struct Point2{T <: Real} <: AbstractPoint
+    x::T
+    y::T
+    𝑈::Frame2{T,<:AbstractString} = 𝑊(T)
 end
 
-# representation of 2D relative pose, frame {𝑢} wrt frame {𝑣} or rigid body motion from
-# {𝑢} to {𝑣}. note that, by convention, θ increases in the anticlockwise direction. the
-# default frame is the world frame. this entity lives in SE(2).
-@with_kw mutable struct Pose2 <: AbstractPose
-    x::Real
-    y::Real
-    θ::Real
-    𝑢::Frame2 = 𝑊
-    𝑣::Frame2 = 𝑊
+# type alias for a 2D point (inferred as a bound-to-frame vector)
+const Vector2 = Point2
+
+# representation of 2D relative pose, ᵛξᵤ, head frame {𝑈} wrt tail frame {𝑉} or rigid body
+# motion from {𝑈} to {𝑉}. the default reference frame {𝑉} is the world frame. the 2D
+# pose entity lives in SE(2).
+@with_kw mutable struct Pose2{T <: Real} <: AbstractPose
+    𝑈::Frame2{T,<:AbstractString} = Frame2{T,String}(0, 0, 0, "unnamed") # Frame{pose head}
+    𝑉::Frame2{T,<:AbstractString} = 𝑊(T) # Frame{pose tail/base (reference)}
+end
+
+# explicit representation of a zero pose
+@with_kw struct Zero2{T <: Real} <: AbstractPose
+    𝑈::Frame2{T,<:AbstractString} = Frame2{T,String}(0, 0, 0, "zero")
+    𝑉::Frame2{T,<:AbstractString} = 𝑈
+end
+
+# returns a zero relative pose of type T
+𝛰(T) = Zero2{T}()
+
+# type alias for union of all 2D geometric entities
+const GeometricEntity2D = Union{Pose2,Point2,Frame2,Zero2}
+
+
+"""
+Custom constructors and field accessors for pose and point.
+"""
+
+# Pose2(x,y,θ; name=head_frame_name, 𝑉=base_frame)
+function Pose2(x::T, y::T, θ::T; name::S="unnamed", 𝑉::AbstractFrame=𝑊(T)) where
+    {T <: Real,S <: AbstractString}
+    𝑈 = Frame2(x, y, θ, name)
+    Pose2(𝑈, 𝑉)
+end
+
+# Point2(x,y)
+function Point2(x::T, y::T) where {T <: Real}
+    Point2(x, y, 𝑊(T))
+end
+
+# Point2(ᵛξᵤ)
+function Point2(ξ::P) where {P <: AbstractPose}
+    Point2(ξ.x, ξ.y, ξ.𝑉)
+end
+
+# Pose2 custom field accessors for ease of use (Pose2.{x,y,θ})
+function getproperty(ξ::P, field::Symbol) where {P <: AbstractPose}
+    if field ∈ (:𝑈, :𝑉)     # head/base frames
+        return getfield(ξ, field)
+    elseif field === :x     # x coordinate
+        return getfield(getfield(ξ, :𝑈), :x)
+    elseif field === :y     # y coordinate
+        return getfield(getfield(ξ, :𝑈), :y)
+    elseif field === :θ     # angle θ (ccw); head frame {𝑈} to relative to base frame {𝑉}
+        return getfield(getfield(ξ, :𝑈), :θ)
+    elseif field === :name  # name of the head frame
+        return getfield(getfield(ξ, :𝑈), :name)
+    else
+        error("Property $field for $ξ not defined!")
+    end
 end
 
 
 """
-Pose operations
+Pose and point operations and algebra:
+1. ᵗξᵤ ⊕ ᵘξᵥ = ᵗξᵥ
+2. ξ₁ ⊕ ξ₂ ≠ ξ₂ ⊕ ξ₁
+3. ⊖ ᵗξᵤ = ᵘξₜ
+4. ξ ⊖ ξ = 𝛰; ⊖ ξ ⊕ ξ = 𝛰
+5. ξ ⊖ 𝛰 = ξ; ⊖ ξ ⊕ 𝛰 = 𝛰
+6. ᵗξᵤ ⋅ ᵘp = ᵗp
 """
 
-# translate (SE2)
-# rotate (SE2)
-# frame change, pose diff, pose add, pose struct
-# ᵛp = ᵛξᵤ ⋅ ᵘp -> dot operator for Pose to point. Will assert base frames to be the same
-# ᵛξₜ = ᵛξᵤ ⊕ ᵘξₜ -> oplus operator for pose to pose. Will assert base frames to be the same
-# minus operator on pose and point2, relying on the unary - operator defined for both
+# oplus operator for pose
+function ⊕(ξ₁::AbstractPose, ξ₂::AbstractPose)
+    if ξ₁ isa Zero2
+        return ξ₂
+    elseif ξ₂ isa Zero2
+        return ξ₁
+    else
+        @assert (ξ₁.𝑈 == ξ₂.𝑉)
+        return (ξ₁.𝑉 == ξ₂.𝑈) ? Pose2(ξ₂.𝑈, ξ₁.𝑉) : 𝛰(typeof(ξ₁.x))
+    end
+end
+
+# ominus unary operator for pose
+function -(ξ::P) where {P <: AbstractPose}
+    return Pose2(ξ.𝑉, ξ.𝑈) # flip reference and head frames
+end
+
+# ominus unary operator for pose
+function -(ξ₁::AbstractPose, ξ₂::AbstractPose)
+    return ξ₁ ⊕ -(ξ₂)
+end
+
+function ⋅(ξ::Pₛ, p::Pₜ) where {Pₛ <: AbstractPose,Pₜ <: AbstractPoint}
+    #
+end
+
+function +(p₁::Point2, p₂::Point2)
+    @assert p₁.𝑈 == p₂.𝑈
+    return Point2(p₁.x + p₂.x, p₁.y + p₂.y, p₁.𝑈)
+end
+
+function -(p₁::P, p₂::P) where {P <: AbstractPoint}
+    @assert p₁.𝑈 == p₂.𝑈
+    return Point2(p₁.x - p₂.x, p₁.y - p₂.y, p₁.𝑈)
+end
+
+# returns the Lᵖ-norm of a 2D pose or point. extending the base linear algebra method.
+function LinearAlgebra.norm(p2::GeometricEntity2D; p::Real=2)
+    return LinearAlgebra.norm([p2.x, p2.y], p)
+end
 
 
 """
@@ -83,13 +178,13 @@ end
 
 
 """
-map setup utils
+Map setup utils
 """
 
 # returns a matrix of size w/res × l/res, filling elements between and including the two
 # scalar functions f and g with the high value. rest of the map above f is filled with low
 # value and below g is filled with the undefined value. note: g(x) ≤ f(x) for int x in 1:n
-# and all length (l), width (w), and resolution (res) are in meters.
+# and the arguments length (l), width (w), and resolution (res) are in meters.
 function generate_map(l::Real, w::Real, f::Function; g::Function=zero, res=5e-2, low=0.0,
     high=1.0, und=Inf64)
     # get map dims; initialize map to free space
@@ -106,12 +201,12 @@ function generate_map(l::Real, w::Real, f::Function; g::Function=zero, res=5e-2,
         @inbounds map[1:f_idx, x] = obj_vec # slice in object val vector
     end
 
-    return Map(map, res, low, high, und)
+    return Map(map, low, high, und, res)
 end
 
 
 """
-map plotting utils
+Map and pose plotting utils
 """
 
 # plots map matrix as a heatmap with appropriate dimensions along the axes. Δx and Δy are
@@ -125,4 +220,22 @@ function plot_map(map::Map; Δx=0.5, Δy=0.5, xₛ=0.0, yₛ=0.1)
     yticks!([(yₛ / res):(Δy / res):m; m], [yₛ:Δy:(m * res); (m * res)] .|> string)
     xlabel!("Length (m)")
     ylabel!("Width (m)")
+end
+
+function plot_pose()
+    #
+end
+
+function plot_obj_ref_frames()
+    #
+end
+
+function plot_scan_ref_frames()
+    #
+end
+
+function plot_scan_heuristic(map::Map)
+    plot_map(map)
+    plot_obj_ref_frames()   # optional
+    plot_scan_ref_frames()  # optional
 end
