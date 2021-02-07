43c13b3e19d0e4e7e4ca69b1c45336edcaf7bc22
Pranshu Malik <pranshumalik14@gmail.com>
2021-02-06 19:16:20 -0500

    starting ik experiment

---
+++ rethinking_inverse_kinematics/rethinking_ik.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 903fcaac-68d8-11eb-3914-87b6029cd760
+
+
+# ╔═╡ Cell order:
+# ╠═903fcaac-68d8-11eb-3914-87b6029cd760
